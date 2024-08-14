8d4c579f2f048f934f1a58885198f2fccbaeeebd
nwaniayo <nwaniayomilala@gmail.com>
2024-08-14 15:59:23 +0100

    chore: Update npm dependencies and add Stripe SDK for payment integration

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
+const formatAmountForStripe = amount => {
+    return Math.round(amount * 100);
+}
 export async function POST(req){
   
     const params = {

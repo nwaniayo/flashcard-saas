9f89d44f898642e9be55984d8a5694ffb6508d38
nwaniayo <nwaniayomilala@gmail.com>
2024-08-14 15:32:27 +0100

    chore: Update npm dependencies and add Firebase and Stripe SDKs

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAurY8PgEnalSX66CzUdvAyXnwIHh0SZUo",
+  authDomain: "flashcardsaas-d22ee.firebaseapp.com",
+  projectId: "flashcardsaas-d22ee",
+  storageBucket: "flashcardsaas-d22ee.appspot.com",
+  messagingSenderId: "527223632672",
+  appId: "1:527223632672:web:b62d9116058bb935609d45",
+  measurementId: "G-6ST48XH2CX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
